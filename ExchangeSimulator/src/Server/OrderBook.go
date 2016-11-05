73955def306fb43bbce202bc0ca668462626dbd5
SohoCompany <zhenzhizhuojian@gmail.com>
2016-11-05 18:36:31 +0800

    OrderBook新增接口，分Bid和Offer提供所有Order

--- ExchangeSimulator/src/Server/OrderBook.go
+++ ExchangeSimulator/src/Server/OrderBook.go
@@ -9,6 +9,8 @@ type OrderBook interface {
 	DelOrder(order Order)
 	FindOrder(orderId string) Order
 	AllOrders() map[string]Order
+	BidOrders() []Order
+	OfferOrders() []Order
 	ProductId() int
 	SetProductId(int)
 	Init()
@@ -26,16 +28,37 @@ func (o *OrderBookImpl) AddOrder(order Order) {
 	//TODO 将order数据存入数据库中的orderBook 数据库操作失败是要抛异常的
 	o._orderMap[order.OrderId()] = order
 	if (order.OrderType() == Bid) {
-		o._bidOrderSeq[len(o._bidOrderSeq)] = order
+		o._bidOrderSeq = append(o._bidOrderSeq, order)
 	} else {
-		o._offerOrderSeq[len(o._offerOrderSeq)] = order
+		o._offerOrderSeq = append(o._offerOrderSeq, order)
 	}
 }
 
 func (o *OrderBookImpl) DelOrder(order Order) {
 	//TODO 将order数据从数据库中的orderBook删除 数据库操作失败是要抛异常的
+	_, exist := o._orderMap[order.OrderId()]
+	if (exist == false) {
+		return 
+	}
 	delete(o._orderMap, order.OrderId())
 	//TODO 从序列中将Order删除
+	if (order.OrderType() == Bid) {
+		var index = 0
+		for ; index < len(o._bidOrderSeq); index++ {
+			if (o._bidOrderSeq[index].OrderId() == order.OrderId()) {
+				break
+			}
+		}
+		o._bidOrderSeq = append(o._bidOrderSeq[:index], o._bidOrderSeq[index:]...)
+	} else {
+		var index = 0
+		for ; index < len(o._offerOrderSeq); index++ {
+			if (o._offerOrderSeq[index].OrderId() == order.OrderId()) {
+				break
+			}
+		}
+		o._offerOrderSeq = append(o._offerOrderSeq[:index], o._offerOrderSeq[index:]...)
+	}
 }
 
 func (o *OrderBookImpl) FindOrder(orderId string) Order {
@@ -52,6 +75,14 @@ func (o *OrderBookImpl) AllOrders() map[string]Order {
 	return o._orderMap
 }
 
+func (o *OrderBookImpl) BidOrders() []Order {
+	return o._bidOrderSeq
+}
+
+func (o *OrderBookImpl) OfferOrders() []Order {
+	return o._offerOrderSeq
+}
+
 func (o *OrderBookImpl) ProductId() int {
 	return o._productId
 }
@@ -61,10 +92,10 @@ func (o *OrderBookImpl) SetProductId(productId int) {
 }
 
 func (o *OrderBookImpl) Init() {
-	//TODO 从数据库加载数据
 	o._orderMap = make(map[string]Order)
 	o._bidOrderSeq = make([]Order, 0)//TODO 正确性有待检验
 	o._offerOrderSeq = make([]Order, 0)//TODO 正确性有待检验
+	//TODO 从数据库加载数据
 }
 
 func CreateOrderBook(productId int) OrderBook{
