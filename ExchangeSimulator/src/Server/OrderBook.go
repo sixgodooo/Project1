7d320a3eaa6732478ec0d186bee3e8a4e642e074
SohoCompany <zhenzhizhuojian@gmail.com>
2016-11-05 17:47:33 +0800

    更新OrderBook实现，新增对Order序列的排序工具

--- ExchangeSimulator/src/Server/OrderBook.go
+++ ExchangeSimulator/src/Server/OrderBook.go
@@ -1,7 +1,7 @@
 package Server
 
 import (
-
+	//"sort"
 )
 
 type OrderBook interface {
@@ -18,16 +18,24 @@ type OrderBookImpl struct {
 	//TODO
 	_productId int
 	_orderMap map[string]Order
+	_bidOrderSeq []Order
+	_offerOrderSeq []Order
 }
 
 func (o *OrderBookImpl) AddOrder(order Order) {
 	//TODO 将order数据存入数据库中的orderBook 数据库操作失败是要抛异常的
 	o._orderMap[order.OrderId()] = order
+	if (order.OrderType() == Bid) {
+		o._bidOrderSeq[len(o._bidOrderSeq)] = order
+	} else {
+		o._offerOrderSeq[len(o._offerOrderSeq)] = order
+	}
 }
 
 func (o *OrderBookImpl) DelOrder(order Order) {
 	//TODO 将order数据从数据库中的orderBook删除 数据库操作失败是要抛异常的
 	delete(o._orderMap, order.OrderId())
+	//TODO 从序列中将Order删除
 }
 
 func (o *OrderBookImpl) FindOrder(orderId string) Order {
@@ -54,10 +62,45 @@ func (o *OrderBookImpl) SetProductId(productId int) {
 
 func (o *OrderBookImpl) Init() {
 	//TODO 从数据库加载数据
+	o._orderMap = make(map[string]Order)
+	o._bidOrderSeq = make([]Order, 0)//TODO 正确性有待检验
+	o._offerOrderSeq = make([]Order, 0)//TODO 正确性有待检验
 }
+
 func CreateOrderBook(productId int) OrderBook{
 	orderBook := new(OrderBookImpl)
 	orderBook.SetProductId(productId)
 	orderBook.Init()
 	return orderBook
 }
+
+/*
+func (o *OrderBookImpl) SortOrder(compare func (order1, order2 *Order) bool) {
+	o._orderSeq = make([]Order, len(o._orderMap))
+	i := 0
+	for _, value := range o._orderMap {
+		o._orderSeq[i] = value
+		i++	
+	}
+	sort.Sort(OrderSorter{o._orderSeq, compare})
+}
+*/
+
+type OrderSorter struct {
+	_orders []Order
+	_compare func(order1, order2 *Order) bool
+}
+
+func (orderSorter *OrderSorter) Len() int {
+	return len(orderSorter._orders)
+}
+
+func (orderSorter *OrderSorter) Swap(i, j int) {
+	orderSorter._orders[i], orderSorter._orders[j] = orderSorter._orders[j], orderSorter._orders[i]
+}
+
+func (orderSorter *OrderSorter) Less(i, j int) bool {
+	return orderSorter._compare(&orderSorter._orders[i], &orderSorter._orders[j])
+}
+
+//OrderSorter排序的使用方法，sort.Sort( OrderSorter{orders, func (order1, order2 *Order) bool { 这里是对两个order的比较} } )
